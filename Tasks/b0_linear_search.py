bfd076b9ef39442cf35c5b04ca977d6d4af6c033
Меньшиков Даниил Александрович <DANMEN@avalon.ru>
2019-04-24 21:29:12 +0300

    Линейный поиск

--- Tasks/b0_linear_search.py
+++ Tasks/b0_linear_search.py
@@ -4,11 +4,18 @@ This module implements some functions based on linear search algo
 
 
 def min_search(arr) -> int:
-	"""
-	Function that find minimal element in array
+    """
 
-	:param arr: Array containing numbers
-	:return: index of first occurrence of minimal element in array
-	"""
+Function that find minimal element in array
 
-	return -1
+:param arr: Array containing numbers
+:return: index of first occurrence of minimal element in array
+"""
+
+    min_ind = 0
+
+    for i in range(1, int(len(arr))):
+        if arr[i] < arr[min_ind]:
+            min_ind = i
+
+    return min_ind
