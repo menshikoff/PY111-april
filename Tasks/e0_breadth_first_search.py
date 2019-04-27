300b3a7359de6c648b7b1c10932fc55540e7dc3b
Меньшиков Даниил Александрович <DANMEN@avalon.ru>
2019-04-27 13:48:31 +0300

    Poisk v shir

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,5 +1,8 @@
 from typing import Any
 import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import Tasks.a1_my_queue as mq
 
 
 def bfs(g: nx.Graph, start_node: Any) -> list:
@@ -10,5 +13,34 @@ def bfs(g: nx.Graph, start_node: Any) -> list:
 	:param start_node: starting node for search
 	:return: list of nodes in the visited order
 	"""
+
+	visit = [start_node]
+	mq.enqueue([start_node])
+	dict_ = {x: [] for x in g.nodes}
+
+	while mq.my_queue:
+		edges_ = list(g.edges(mq.dequeue()))
+
+		for item in edges_:
+			if item[1] not in visit:
+				visit.append(item[1])
+				dict_[item[1]] = dict_[item[0]] + list(item[0])
+				mq.enqueue(item[1])
+
 	print(g, start_node)
-	return list(g.nodes)
+	print(dict_)
+	return visit
+
+
+if __name__ == '__main__':
+
+	g = nx.Graph()
+	l_nodes = ['A', 'B', 'C', 'D', 'E']
+	g.add_nodes_from(l_nodes)
+	g.add_edge('A', 'B')
+	g.add_edge('A', 'C')
+	g.add_edge('B', 'D')
+	g.add_edge('C', 'D')
+	g.add_edge('D', 'E')
+
+	print(bfs(g, 'A'))
