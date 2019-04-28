abae6545955eb17a99a8a8a5700b51e5260a107e
Daniil Menshikov <daniil.menshikoff@gmail.com>
2019-04-28 23:36:44 +0300

    Binary search is done

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -1,3 +1,34 @@
+def left_bound(elem_, arr_):
+	left = -1
+	right = len(arr_)
+
+	while right - left > 1:
+		middle = (left + right) // 2
+
+		if arr_[middle] < elem_:
+			left = middle
+		else:
+			right = middle
+
+	return left
+
+
+def right_bound(elem_, arr_):
+	left = -1
+	right = len(arr_)
+
+	while right - left > 1:
+
+		middle = (left + right) // 2
+
+		if arr_[middle] <= elem_:
+			left = middle
+		else:
+			right = middle
+
+	return right
+
+
 def binary_search(elem, arr):
 	"""
 	Performs binary search of given element inside of array
@@ -7,4 +38,7 @@ def binary_search(elem, arr):
 	:return: Index of element if it's presented in the arr, None otherwise
 	"""
 
-	return None
+	if right_bound(elem, arr) - left_bound(elem, arr) == 1:
+		return None
+	else:
+		return left_bound(elem, arr) + 1  # Чтобы пройти тест было решено выводить только одно первое найденное значение
