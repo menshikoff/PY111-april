85b00fbbe38cc2ca357965af6f3311f1bccfed13
Daniil Menshikov <daniil.menshikoff@gmail.com>
2019-04-28 20:58:40 +0300

    Brackets is done

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -1,8 +1,25 @@
+import Tasks.a0_my_stack as stack
+
+
 def check_brackets(brackets_row: str) -> bool:
+
 	"""
 	Check whether input string is a valid bracket sequence
 	:param brackets_row: input string to be checked
 	:return: True if valid, False otherwise
 	"""
 
-	return False
+	stack.clear()
+
+	for char in brackets_row.__iter__():
+		if char == ')' and not stack.my_stack:
+			return False
+		if char == '(':
+			stack.push(char)
+		if char == ')' and stack.my_stack:
+			stack.pop()
+
+	if stack.my_stack:
+		return False
+
+	return True
