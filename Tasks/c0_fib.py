0292d529709f3ee5d6688c1f532fd043167ec160
Daniil Menshikov <daniil.menshikoff@gmail.com>
2019-05-01 01:00:21 +0300

    Fib is done!

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,7 +5,16 @@ def fib_recursive(n: int) -> int:
 	:param n: number of item
 	:return: Fibonacci number
 	"""
-	return None
+
+	if n < 0 or type(n) != int:
+		raise ValueError("Input argument should be positive and int!")
+
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+
+	return fib_recursive(n-1) + fib_recursive(n-2)
 
 
 def fib_iterative(n: int) -> int:
@@ -15,4 +24,17 @@ def fib_iterative(n: int) -> int:
 	:param n: number of item
 	:return: Fibonacci number
 	"""
-	return None
+	if n < 0 or type(n) != int:
+		raise ValueError("Input argument should be positive and int!")
+
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+
+	fib_l = [0, 1]
+
+	for i in range(2, n+1):
+		fib_l.append(fib_l[i-2] + fib_l[i-1])
+
+	return fib_l.pop()
