42c9ecdf035fc61af9fa2c4f6517dfa380e36dc4
Daniil Menshikov <daniil.menshikoff@gmail.com>
2019-05-09 13:44:19 +0300

    ladder

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -5,4 +5,15 @@ def stairway_path(stairway: list) -> int:
 	:param stairway: list of ints, where each int is a cost of appropriate step
 	:return: minimal cost of getting to the top
 	"""
-	return None
+
+	cost = [stairway[0], stairway[1]]
+
+	for i in range(2, len(stairway)):
+		cost.append(stairway[i]+min(cost[i-1], cost[i-2]))
+
+	return cost[-1]
+
+
+if __name__ == '__main__':
+
+	print(stairway_path([5, 11, 43, 2, 23, 43, 22, 12, 6, 8]))
