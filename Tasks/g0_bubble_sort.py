d011f7464fc0f701c30dd8061cf9f234d93d63b7
daniel <daniil.menshikoff@gmail.com>
2019-07-18 20:15:37 +0300

    bubble is done

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -4,10 +4,26 @@ _Tt = TypeVar("T")
 
 
 def sort(container: Sequence[_Tt]) -> Sequence[_Tt]:
-	"""
-	Sort input container with bubble sort
+    """
+    Sort input container with bubble sort
 
-	:param container: container of elements to be sorted
-	:return: container sorted in ascending order
-	"""
-	return container
+    :param container: container of elements to be sorted
+    :return: container sorted in ascending order
+    """
+
+    for i in range(len(container)-1):
+
+        n = 0
+
+        while not n == len(container)-1:
+            if container[n] > container[n+1]:
+                container[n], container[n+1] = container[n+1], container[n]
+            n += 1
+
+    return container
+
+
+if __name__ == '__main__':
+
+    print(sort([10, 4, 5, 6, 9, 8, 56, 67, 0, -2]))
+
